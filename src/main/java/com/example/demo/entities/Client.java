bdcd0ccb22cc30c8dd21daf43682871e7363ee68
Marcos Marins <marcos-henrique2000@hotmail.com>
2020-09-30 11:10:19 -0300

    iniciando projeto com entitades

---
+++ src/main/java/com/example/demo/entities/Client.java
@@ -0,0 +1,68 @@
+package com.example.demo.entities;
+
+import com.example.demo.entities.enums.Department;
+
+public class Client {
+	
+	private Integer idClient;
+	private String name;
+	private Department department;
+	
+	public Client() {}
+
+	public Client(Integer idClient, String name, Department department) {
+		super();
+		this.idClient = idClient;
+		this.name = name;
+		this.department = department;
+	}
+
+	public Integer getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(Integer idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idClient == null) ? 0 : idClient.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (idClient == null) {
+			if (other.idClient != null)
+				return false;
+		} else if (!idClient.equals(other.idClient))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/com/example/demo/entities/Order.java
@@ -0,0 +1,87 @@
+package com.example.demo.entities;
+
+import java.util.Date;
+
+public class Order {
+	private Integer idOrder;
+	private String title;
+	private String description;
+	private Date dateOrder;
+	private Client client;
+	
+	public Order() {}
+
+	public Order(Integer idOrder, String title, String description, Date dateOrder, Client client) {
+		super();
+		this.idOrder = idOrder;
+		this.title = title;
+		this.description = description;
+		this.dateOrder = dateOrder;
+		this.client = client;
+	}
+
+	public Integer getIdOrder() {
+		return idOrder;
+	}
+
+	public void setIdOrder(Integer idOrder) {
+		this.idOrder = idOrder;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDateOrder() {
+		return dateOrder;
+	}
+
+	public void setDateOrder(Date dateOrder) {
+		this.dateOrder = dateOrder;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idOrder == null) ? 0 : idOrder.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (idOrder == null) {
+			if (other.idOrder != null)
+				return false;
+		} else if (!idOrder.equals(other.idOrder))
+			return false;
+		return true;
+	}
+}
